5303a342adf05e00d89d753cab7abef572884086
pmc76222 <pmc76222@gmail.com>
2022-12-04 14:25:43 +0900

    배경

--- js/background.js
+++ js/background.js
@@ -1,12 +1,12 @@
 
 
 
-const images = ['https://blogfiles.pstatic.net/20130419_287/frenchaffair_1366353419579ONPPi_JPEG/ekf11.jpg'
-                ,'https://blogfiles.pstatic.net/20130419_181/frenchaffair_1366353419280iLaVq_JPEG/ekf8.jpg'
-                
+const images = ['ekf4.jpg',
+                'ekf8.jpg',
+                'ekf11.jpg'                
             ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-document.body.style.backgroundImage = `url(${chosenImage})`;
+document.body.style.backgroundImage = `url(images/${chosenImage})`;
 
